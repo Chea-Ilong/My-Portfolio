ca4e17553e4a6e0195777cd09364567e74294add
Chea Ilong <cheadara133@gmail.com>
2026-02-27 15:03:20 +0700

    fix Blocked due to other 4xx issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,35 +11,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'monthly',
             priority: 1,
         },
-        {
-            url: `${baseUrl}/#about`,
-            lastModified,
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: `${baseUrl}/#experience`,
-            lastModified,
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: `${baseUrl}/#skills`,
-            lastModified,
-            changeFrequency: 'monthly',
-            priority: 0.7,
-        },
-        {
-            url: `${baseUrl}/#projects`,
-            lastModified,
-            changeFrequency: 'monthly',
-            priority: 0.9,
-        },
-        {
-            url: `${baseUrl}/#contact`,
-            lastModified,
-            changeFrequency: 'monthly',
-            priority: 0.7,
-        },
     ]
 }
